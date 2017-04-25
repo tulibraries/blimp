ce226a38aaeac78fe4d41f6b6ed54b47c6645830
Steven Ng <steven.ng@temple.edu>
2017-04-25 14:11:19 -0400

    Fix typo

--- app/models/blimp/upload.rb
+++ app/models/blimp/upload.rb
@@ -1,6 +1,6 @@
 module Blimp
   class Upload < ApplicationRecord
-    valdates_presence_of :datafile
+    validates_presence_of :datafile
     mount_uploader :datafile , DatafileUploader
   end
 end
